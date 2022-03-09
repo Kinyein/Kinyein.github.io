f14e3db9b7419a8d33942ab80297b07079ff98b4
Kevin <j1120531@gmail.com>
2022-03-08 21:45:02 -0500

    Clase Calculadora la cual tiene las operaciones basicas de la misma

---
+++ scripts/Calculadora.js
@@ -0,0 +1,14 @@
+class Calculadora {         //Esta es la clase Calculadora, dentro de la clase hay metodos (sumar, restar, dividir, multiplicar)
+    sumar(num1, num2){
+        return num1 + num2;
+    }
+    restar(num1, num2){
+        return num1 - num2;
+    }
+    dividir(num1, num2){
+        return num1 / num2;
+    }
+    multiplicar(num1, num2){
+        return num1 * num2;
+    }
+}
